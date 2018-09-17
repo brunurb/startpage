2c417c8d39dd35162e58d7e280e7d9bca1b6526f
brunurb <brunurb+github@gmail.com>
2018-09-17 04:15:20 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ var linkMenu = [
   ["canva",                    "https://www.canva.com/",""],
   ["spreadshirt",              "https://partner.spreadshirt.co.uk/designs",""],
   ["discord",                  "https://discordapp.com/",""],
-  ["GIFVideo",        "https://ezgif.com/",""],
+  ["GIFVideo",                 "https://ezgif.com/",""],
   
   [svgSocial,                  "green",                                       "-HEAD-"], // Media
   ["YouTube",                  "https://www.youtube.com/",""],
@@ -61,7 +61,7 @@ var linkMenu = [
   ["DeviantArt",               "https://www.deviantart.com/brunurb",""],
   
   [svgClover,                  "cyan",                                        "-HEAD-"], // 4chan
-  ["GIFVideo",        "https://www-inoreader.com/",""],
+  ["FeedReader",               "https://www-inoreader.com/",""],
   ["/g/ Technology",           "",""],
   ["/w/ Anime/Wallpapers",     "",""],
   ["/wg/ Wallpaper/General",   "",""],
